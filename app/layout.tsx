50784c4c59188d34ca212c331713aaf63084004f
Ahn Kiwook <ahnkiwook@gmail.com>
2023-12-23 16:36:34 +0900

    Remove ChatBox and get ready to implement Chazzy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import {Nanum_Gothic} from 'next/font/google'
-import './globals.css'
 
 const nanumGothic = Nanum_Gothic({weight: '700', subsets: ['latin']})
 
 export default function RootLayout({children}) {
     return (
         <html lang="ko">
-        <body className={nanumGothic.className}>
-        {children}
-        </body>
+            <body className={nanumGothic.className} style={{ margin: 0 }}>
+                {children}
+            </body>
         </html>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
 export default function Home() {
     return (
         <main style={{color: "black"}}>
-            <h1>치지직 오버레이</h1>
+            <h1>Chazzy</h1>
             <p>
-                두로님 채팅창을 치지직 버전으로 포팅했습니다.<br/>
+                채팅창 및 도네이션을 보기 쉽게 확인할 수 있는 페이지입니다.<br/>
                 다른 스트리머 분들도 뒤에 주소만 바꾸면 사용이 가능합니다.
             </p>
             <h3>
                 예시
             </h3>
             <p>
-                두로님 치지직 주소: https://chzzk.naver.com/6e06f5e1907f17eff543abd06cb62891<br/>
-                두로님 Twitch 주소: https://www.twitch.tv/nokduro<br/>
-                오버레이 주소: https://chzzk-overlay.vercel.app/6e06f5e1907f17eff543abd06cb62891-nokduro
-            </p>
-            <p>
-                추가로, 오버레이 주소 뒤에 <code>?small</code> 을 붙이면 채팅 간격이 넓어집니다.
+                녹두로님 치지직 주소: https://chzzk.naver.com/6e06f5e1907f17eff543abd06cb62891<br/>
+                녹두로님 Twitch 주소: https://www.twitch.tv/nokduro<br/>
+                Chazzy 주소: https://chazzy.vercel.app/6e06f5e1907f17eff543abd06cb62891-nokduro
             </p>
         </main>
     )
