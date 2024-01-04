c99b0211d5228c1b9c5bf9881e347994e1ddeb43
Ahn Kiwook <ahnkiwook@gmail.com>
2024-01-04 19:16:26 +0900

    Add Vercel analytics to maintenance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
+import {Analytics} from "@vercel/analytics/react"
 import "./globals.css"
 
 export default function RootLayout({children}) {
     return (
         <html lang="ko">
-        <head>
-            <title>Chazzy</title>
-        </head>
-        <body style={{margin: 0}}>
-        {children}
-        </body>
+            <head>
+                <title>Chazzy</title>
+            </head>
+            <body style={{margin: 0}}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     )
 }
